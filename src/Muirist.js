024aa13d18240c0c64f4eb15cf204f09785a0699
Ben Gregory <JBGregoryPro@protonmail.com>
2021-12-03 14:12:36 -0600

    initial module buildout and setup

---
+++ src/Muirist.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './Muirist.css';
+
+function Muirist() {
+  return (
+    <div className="Muirist">
+    Test page for testing
+    </div>
+  );
+}
+
+export default Muirist;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Muirist from './Muirist';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Muirist />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
